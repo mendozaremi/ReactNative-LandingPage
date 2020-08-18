69b9a4b92fd72681042bd321b507e5234b8c98a8
Remi Mendoza <mendozaremi@gmail.com>
2020-08-18 14:21:51 -0400

    completed landing page

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+import colors from '../config/colors';
+
 export default function ViewImageScreen() {
   return (
     <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.closeIcon}></View>
       <Image
         resizeMode="contain"
-        style={}
+        style={styles.image}
         source={require('../assets/chair.jpg')}
       />
     </View>
@@ -14,9 +18,28 @@ export default function ViewImageScreen() {
 }
 
 const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: 'absolute',
+    top: 50,
+    left: 50,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: 'absolute',
+    top: 50,
+    right: 30,
+  },
   image: {
     width: '100%',
     height: '100%',
   },
-  container: {},
+  container: {
+    backgroundColor: '#000',
+    flex: 1,
+  },
 });
